9bd73914e64d94da8116b4c427e0cb41b3b70d99
Paul Roger <pauger.rol@gmail.com>
2023-02-02 15:29:37 +0100

    Angulardon - likehate - oups

---
+++ src/app/shared/components/colleague/colleague.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tc-colleague',
+  templateUrl: './colleague.component.html',
+  styleUrls: ['./colleague.component.scss']
+})
+export class ColleagueComponent {
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LikeHateComponent } from '../shared/components/like-hate/like-hate.component';
+import { ColleagueComponent } from '../shared/components/colleague/colleague.component';
 @NgModule({
   declarations: [
-    LikeHateComponent
+    LikeHateComponent,
+    ColleagueComponent
   ],
   exports: [
     LikeHateComponent
