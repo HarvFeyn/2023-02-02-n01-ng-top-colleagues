667e0ecca9b88b26e2e8c0e7b34bb5b504ed2be0
Paul Roger <pauger.rol@gmail.com>
2023-02-03 10:04:53 +0100

    Angulardon - colleague pipe score

---
+++ src/app/shared/pipes/score.pipe.ts
@@ -0,0 +1,18 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'score'
+})
+export class ScorePipe implements PipeTransform {
+  transform(value: number) :string {
+    let result:string;
+    if(value >= 0) {
+      result = "+ ".concat(value.toString());
+      return result;
+    } else {
+      result = "- ".concat((value*-1).toString());
+      return result;
+    }
+  }
+
+}
