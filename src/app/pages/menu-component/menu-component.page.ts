86eeddbf36759b793363149152cabd13f188d018
Paul Roger <pauger.rol@gmail.com>
2023-02-09 10:37:10 +0100

    Angulardon - router

---
+++ src/app/pages/menu-component/menu-component.page.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tc-menu-component',
+  templateUrl: './menu-component.page.html',
+  styleUrls: ['./menu-component.page.scss']
+})
+export class MenuComponentPage {
+
+}
